8e43e5a9af8e6f7d8fe29e2a3fc54b1db2e18846
John W <john.weaver94@utexas.edu>
2022-12-20 19:36:13 -0600

    Added module.exports to our books model

--- models/Books.js
+++ models/Books.js
@@ -1,8 +1,8 @@
-const { Model, DataTypes, STRING } = require('sequelize');
-const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Books extends Model {}
+
 Books.init(
   {
     id: {
@@ -29,5 +29,21 @@ Books.init(
         type: DataTypes.DATE,
         allowNull: false,
         defaultValue: DataTypes.NOW
-    }
-});
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Books',
+  }
+);
+module.exports = Books;
